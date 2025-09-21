893b02d98fae85a40118c05910ef2356db8a4c0c
Prasad <prasadzoman15@gmail.com>
2025-09-21 23:00:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.set('trust proxy', 1);
 // ------------------ MIDDLEWARE ------------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://agastyatrace-client.onrender.com",
     credentials: true,
   })
 );
